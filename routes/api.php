99d5282f520fdb41d55a7c14ef4d76c82ea38755
Millen Mark Aquino <millenmark.dev@gmail.com>
2023-08-05 19:49:24 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Sample Routes
+Route::post('/sample', 'SampleController@store')->name('sample');
